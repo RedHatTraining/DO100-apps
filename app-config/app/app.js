6713c84b3a51dbb1b05e2f7535b40bdb5fb34bf6
Eduardo Ramirez <42619854+ebramirez@users.noreply.github.com>
2021-11-06 21:23:26 +0100

    Apply suggestions from code review
    Co-authored-by: Guy Bianco IV <gjbiancoiv@gmail.com>

--- app-config/app/app.js
+++ app-config/app/app.js
@@ -1,28 +1,21 @@
-var express = require('express');
-var fs = require('fs')
-app = express();
+const express = require('express');
+const fs = require('fs');
+const app = express();
 
 // read in the APP_MSG env var
-var msg = process.env.APP_MSG;
-
-var response;
+const response = `Value in the APP_MSG env var is => ${process.env.APP_MSG}\n`;
 
 app.get('/', function (req, res) {
-
-    response = 'Value in the APP_MSG env var is => ' + msg + '\n';
 
     // Read in the secret file
     fs.readFile('/opt/app-root/secure/myapp.sec', 'utf8', function (secerr,secdata) {
         if (secerr) {
             console.log(secerr + '\n');
-            response += secerr + '\n';
+          res.send(response + secerr + '\n');
         }
         else {
-            response += 'The secret is => ' + secdata + '\n';
+            res.send(`${response}The secret is => ${secdata}\n`;
         }
-
-        //send the response to the client
-        res.send(response);
     });
 
 });
